782c48515fd574d70a3217e2f8e12f8c5bfc45e4
Promise278 <promiseobi2007@gmail.com>
2026-05-06 13:11:37 +0100

    fixs: updated test file

--- test/Lock.js
+++ test/Lock.js
@@ -1,12 +1,10 @@
 const { expect } = require("chai");
 
 describe("TodoList Contract", function () {
-  let Todo, todo, owner;
+  let todo;
 
   beforeEach(async function () {
-    [owner] = await ethers.getSigners();
-
-    Todo = await ethers.getContractFactory("TodoList");
+    const Todo = await ethers.getContractFactory("TodoList");
     todo = await Todo.deploy();
     await todo.waitForDeployment();
   });
@@ -14,17 +12,16 @@ describe("TodoList Contract", function () {
   it("should add a task", async function () {
     await todo.addTask("Learn Solidity");
 
-    const tasks = await todo.getMyTasks();
+    const task = await todo.getTask(0);
 
-    expect(tasks.length).to.equal(1);
-    expect(tasks[0].content).to.equal("Learn Solidity");
-    expect(tasks[0].completed).to.equal(false);
+    expect(task.todo).to.equal("Learn Solidity");
+    expect(task.completed).to.equal(false);
   });
 
   it("should toggle task completion", async function () {
-    await todo.addTask("Build Web3 app");
+    await todo.addTask("Learn Solidity");
 
-    await todo.toggleTask(0);
+    await todo.toggleCompleted(0);
 
     const task = await todo.getTask(0);
 
