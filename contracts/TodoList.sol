fdda34c8fcde10b0241a47a762db6c5e7f58158e
Promise278 <promiseobi2007@gmail.com>
2026-05-08 10:00:33 +0100

    feat: added .env and changed todolist

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,20 +2,26 @@
 pragma solidity ^0.8.27;
 
 contract TodoList {
-
     struct Task {
+        uint256 id;
         string todo;
         bool completed;
+        uint256 createdAt;
     }
 
     Task[] public tasks;
+    uint256 private nextId;
 
     function addTask(string memory _todo) public {
-        tasks.push(Task(_todo, false));
+        tasks.push(Task({id: nextId, todo: _todo, completed: false, createdAt: block.timestamp}));
     }
 
     function getTask(uint256 _index) public view returns (Task memory) {
         return tasks[_index];
+    }
+
+    function getAllTasks() public view returns (Task[] memory) {
+        return tasks;
     }
 
     function toggleCompleted(uint256 _index) public {
