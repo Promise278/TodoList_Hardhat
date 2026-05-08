0426fa34c85d463d9958971a266c3298ba9af7b7
Promise278 <promiseobi2007@gmail.com>
2026-05-08 10:35:02 +0100

    feat: added script and deploy code to deploy contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-verify");
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
---
+++ script/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const TodoList = await hre.ethers.getContractFactory("TodoList");
+
+  const todoList = await TodoList.deploy();
+
+  await todoList.waitForDeployment();
+
+  console.log("TodoList deployed to:", await todoList.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
